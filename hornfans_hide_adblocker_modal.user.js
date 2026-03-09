3ce0b36c55bcadd97e6ed20a2d1de3f2b99c1e30
sdzkz <sdzkz@users.noreply.github.com>
2026-03-09 13:03:37 -0500

    Rename

--- hornfans_hide_adblocker_modal.user.js
+++ hornfans_hide_adblocker_modal.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         HornFans Modal Closer
+// @name         HornFans - Close Ad Blocker Modal
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Automatically closes the modal on hornfans.com
