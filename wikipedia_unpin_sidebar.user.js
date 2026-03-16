130348678b9a337b37ed30623c2c04383fa899f7
sdzkz <sdzkz@users.noreply.github.com>
2026-03-16 13:11:26 -0500

    wikipedia_unpin_sidebar.user.js

---
+++ wikipedia_unpin_sidebar.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Wikipedia Unpin Sidebar
+// @namespace    https://github.com/billp
+// @version      1.1
+// @description  Clicks the unpin button on the main menu sidebar on page load
+// @match        *://*.wikipedia.org/*
+// @grant        none
+// @run-at       document-idle
+// @updateURL    https://raw.githubusercontent.com/sdzkz/userscripts-git/main/wikipedia_unpin_sidebar.user.js
+// @downloadURL  https://raw.githubusercontent.com/sdzkz/userscripts-git/main/wikipedia_unpin_sidebar.user.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const selector = '#vector-main-menu > div.vector-pinnable-header.vector-main-menu-pinnable-header.vector-pinnable-header-pinned > button.vector-pinnable-header-toggle-button.vector-pinnable-header-unpin-button';
+
+  const btn = document.querySelector(selector);
+  if (btn) {
+    btn.click();
+  }
+})();
